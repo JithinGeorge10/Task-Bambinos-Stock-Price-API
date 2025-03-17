cf3b2f3c13c529e7e89bbc6c8f47ea65cf422ddd
jithingeorgedev <120901504+JithinGeorge10@users.noreply.github.com>
2025-03-18 02:34:47 +0530

    error handling done

--- backend/src/repositories/StockRepository.ts
+++ backend/src/repositories/StockRepository.ts
@@ -3,9 +3,15 @@ import { stockDetailsOutput } from "../interface/repositories/stockRepository.ty
 import StockModel from "../models/Stock";
 
 export class StockRepository implements IStockRepository {
-    getStockPrice = async (symbol: string, date: String): Promise<stockDetailsOutput> =>{
-        const stock = await StockModel.findOne({ symbol, date });
-        console.log(stock)
+    getStockPrice = async (symbol: string, date: String): Promise<stockDetailsOutput> => {
+        const stockSymbol = await StockModel.findOne({ symbol: symbol.toUpperCase() });
+        if(!stockSymbol){
+            throw new Error(`Stock symbol '${symbol}' not found`); 
+        }
+        const stock = await StockModel.findOne({ symbol: symbol.toUpperCase(), date });
+        if (!stock) {
+            throw new Error(`No data available for '${symbol}' on ${date}`); 
+        }
         return { closingPrice: stock?.closing_price ?? null };
     }
 
