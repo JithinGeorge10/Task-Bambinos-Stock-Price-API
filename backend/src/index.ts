2454d37a3c41a8f099d613ad9a01b7b98318a26e
jithingeorgedev <120901504+JithinGeorge10@users.noreply.github.com>
2025-03-18 02:15:00 +0530

    added cors for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import morgan from "morgan";
-import { PORT } from "./utils/constants";
+import { PORT, FRONTEND_URL } from "./utils/constants";
 import { connectDb } from "./config/database";
 import stockRoutes from "./routes/stockRoutes";
+import cors from "cors";
 
 const app = express();
 
-app.use(morgan("dev"));    
+app.use(morgan("dev"));
 app.use(express.json());
 connectDb();
+const corsOptions = {
+    origin: FRONTEND_URL() || "*",
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use("/api",stockRoutes);
+app.use("/api", stockRoutes);
 
-app.listen(PORT,()=>{
-    console.log('started');
+app.listen(PORT, () => {
+    console.log(`Server started running on port ${PORT}`)
 })
--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -4,6 +4,10 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const MONGO_URI = () => {
-    if (!process.env.MONGO_URI) throw new Error("Mongo URI not found in env");
-    return String(process.env.MONGO_URI);
-  };
+  if (!process.env.MONGO_URI) throw new Error("Mongo URI not found in env");
+  return String(process.env.MONGO_URI);
+};
+export const FRONTEND_URL = () => {
+  if (!process.env.FRONTEND_URL) return null;
+  return String(process.env.FRONTEND_URL);
+};
